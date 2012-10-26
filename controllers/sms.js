1321ada53cd569869c394a171a960c45e2d77534
don browning <don.browning@gmail.com>
2012-10-26 14:41:38 -0400

    more twilio stuff

--- controllers/sms.js
+++ controllers/sms.js
@@ -6,11 +6,45 @@ var responseTail = '</Sms></Response>';
 
 
 exports.events = function(req, res){
-	console.log('From ' + req.body.From);
-	console.log('To ' + req.body.To);
-	console.log('Body ' + req.body.Body);
+	var from = req.body.From;
+	var to = req.body.To;
+	var body = req.body.Body.toLowerCase();
 
-	res.send('ok');
+	var message = '';
+
+	console.log('From ' + from);
+	console.log('To ' + to);
+	console.log('Body ' + body);
+
+	switch(true) {
+		case(body === 'events'):
+			var d = new Date('10-27-2012');
+			EventProvider.getEventsForDate(d, function(err, events){
+				message += 'Found ' + events.length + ' events: \n';
+				for(var i = 0;i<events.length;i++) {
+					message += events[i].Name + '\n';
+				}
+				console.log('message ' + message);
+				res.send (message);
+			})
+			
+			break;
+		
+		case(body === 'cnn'):
+			console.log('query based on cnn');
+			break;
+		
+		case(body === 'techwood'):
+			console.log('find techwood events');
+			break;
+		case(body === 'twc'):
+			break;
+		default:
+			res.send(helpString());
+			break;
+	}
+
+	
 	
 }
 
@@ -18,7 +52,10 @@ function createResponse(message){
 	return responseHead + message + responseTail;
 }
 
-
+function helpString() {
+	var message = "Valid commands are: 'events', 'cnn', 'techwood', and 'twc'";
+	return createResponse(message);
+}
 
 exports.index = function(req, res){
 	res.render('twil');
