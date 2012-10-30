f3906ced92086ceb74186e28643a5c90f77694d2
Don Browning <don.browning@gmail.com>
2012-10-29 21:39:14 -0400

    Generated documentation for a082f463c9466587f312e0710521b8e78564beb4

--- assets/behavior.js
+++ assets/behavior.js
@@ -16,8 +16,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         },
         "sourcePath": "/Users/donwb/Desktop/dev/eventful/app.js",
         "projectPath": "app.js",
-        "targetPath": "app",
-        "title": "app"
+        "targetPath": "index",
+        "title": "index"
       },
       "depth": 1,
       "outline": [
