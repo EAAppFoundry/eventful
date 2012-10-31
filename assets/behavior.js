5811daf80ba2120531fe026eda3c307e29c5fdfc
Don Browning <don.browning@gmail.com>
2012-10-30 22:05:18 -0400

    Generated documentation for e5defb079ec86017e18f2cb8ce36f244c846abec

--- assets/behavior.js
+++ assets/behavior.js
@@ -190,7 +190,17 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "title": "event"
           },
           "depth": 2,
-          "outline": []
+          "outline": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "event.js",
+                "slug": "eventjs"
+              },
+              "depth": 2
+            }
+          ]
         }, {
           "type": "file",
           "data": {
